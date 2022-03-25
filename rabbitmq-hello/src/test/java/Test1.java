b0a354d55de81aca77f138570621625e21bd6ea3
Yuneryou <166462894@qq.com>
2022-03-25 14:58:12 +0800

    test first

---
+++ rabbitmq-hello/src/test/java/Test1.java
@@ -0,0 +1,21 @@
+public class Test1 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000; i++) {
+            int sum=0;
+            for (int j = i; j <1000 ; j++) {
+                sum+=j;
+                if (sum>1000){
+                    break;
+                }
+                else if (sum==1000){
+                    for (int k = i; k <j ; k++) {
+                        System.out.print(k+"   ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+
+    }
+}
